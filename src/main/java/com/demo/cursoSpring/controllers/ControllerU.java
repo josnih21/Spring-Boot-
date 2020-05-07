13f89fc3a73f4034b8fff7975b551158420cfb61
José Ángel <josnih@gmail.com>
2020-05-07 17:34:24 +0100

    practicando con request param para pasar paramentros a través del controlador para inyectar en la plantilla HTML

--- src/main/java/com/demo/cursoSpring/controllers/ControllerU.java
+++ src/main/java/com/demo/cursoSpring/controllers/ControllerU.java
@@ -3,12 +3,13 @@ package com.demo.cursoSpring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerU {
     @GetMapping("/")
-    public String welcomePage(Model model){
-        model.addAttribute("mensaje","¡hola!");
+    public String welcomePage(@RequestParam(value = "name", required = false, defaultValue = "Pepe") String name , Model model){
+        model.addAttribute("nombre",name);
         return "template";
     }
 
